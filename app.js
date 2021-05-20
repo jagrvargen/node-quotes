70473c28be7e74824a7fd7c1771d47bfa21739d9
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2021-05-20 06:21:36 -0700

    updated code to use node-fetch and promises

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const request = require('request');
+const fetch = require('node-fetch');
 const readline = require('readline-sync');
 
-let a = Number(readline.question("How many quotes (1-10)?"));
+const endpoint = 'https://api.taylor.rest'
 
-if (a > 10) {
-    a = 10;
+let a = Number(readline.question("\nHow many quotes would you love to live by? (max 10) "));
+
+while (a < 1 || a > 10) {
+    console.log("Please enter a number from 1 to 10");
+    a = Number(readline.question("How many quotes (1-10)?"));
 }
 
-for (let i = 0; i < a; i++) {
-    request('https://api.taylor.rest/', function (error, response, body) {
-        console.log('Quote:', JSON.parse(body)["quote"], "\n");
-    });
-}
+console.log();
+
+Promise.all(Array(a).fill(endpoint).map((url, _) =>
+    fetch(url).then(resp => resp.json())
+    .then(json => console.log(json["quote"]))
+    .catch(error => console.log(error))
+));
