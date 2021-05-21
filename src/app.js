83ddcdae2d09bc737732e517ae9af77a9b343b92
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2021-05-21 16:42:02 -0700

    app is working as described. need to add leaderboard command line argument

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
-const getJoke = require('./routes/get-jokes');
+const returnResponse = require('./routes/get-jokes');
 const process = require('process');
+const fs = require('fs');
+
+const filename = './jokes.txt'
+
+function randInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function validateResponse(response) {
+    let jokes = response.data.results
+    if (typeof jokes === 'undefined' || jokes === null ||  jokes.length === 0) {
+        throw new Error("No joke found");
+    }
+    return jokes[randInt(jokes.length)].joke
+}
+
+function saveJoke(joke) {
+    fs.appendFile(filename, joke, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log(joke)
+    });
+}
 
 if (!process.argv[2]) {
     let prompt = require('prompt');
@@ -7,13 +31,15 @@ if (!process.argv[2]) {
     prompt.start();
 
     prompt.get(['category'], function (err, result) {
-        Promise.resolve(getJoke(result.category))
-        .then(resp => console.log(resp.data))
+        Promise.resolve(returnResponse(result.category))
+        .then(resp => validateResponse(resp))
+        .then(joke => saveJoke(joke))
         .catch(error => console.log(error))
     });
 } else {
     const searchTerm = process.argv[2]
-    Promise.resolve(getJoke(searchTerm))
-        .then(resp => console.log(resp.data))
+    Promise.resolve(returnResponse(searchTerm))
+        .then(resp => validateResponse(resp))
+        .then(joke => saveJoke(joke))
         .catch(error => console.log(error))
 }
--- src/routes/get-jokes.js
+++ src/routes/get-jokes.js
@@ -5,12 +5,23 @@ const url = 'https://icanhazdadjoke.com/';
 
 const jokes = axios.create({
     baseURL: url,
-    headers: {'Accept': 'application/json'}
+    headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'Practice (https://github.com/jagrvargen/node-quotes)'
+    }
 });
 
-module.exports = function getJoke(searchTerm) {
+function getJoke(searchTerm) {
     if (!searchTerm || searchTerm === "") {
         return jokes();
     }
     return jokes(url + 'search?' + querystring.stringify({term: searchTerm}));
+}
+
+module.exports = function returnResponse(searchTerm) {
+    return new Promise(function (resolve, reject) {
+        let jokeJSON = getJoke(searchTerm)
+        resolve(jokeJSON)
+        reject("error")
+    })
 }
