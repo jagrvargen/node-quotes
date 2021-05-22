56f2b66c44bb4a9a8aec0dcfdfc73fa0ad077943
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2021-05-21 18:46:57 -0700

    clear out whitespace from jokes.txt

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ function validateResponse(response) {
 }
 
 function saveJoke(joke) {
-    fs.appendFile(filename, joke, (err) => {
+    fs.appendFile(filename, joke + "\n", (err) => {
         if (err) {
             throw err;
         }
@@ -25,16 +25,50 @@ function saveJoke(joke) {
     });
 }
 
+function findLeader() {
+    fs.readFile(filename, encoding='utf8', (err, data) => {
+        if (err) {
+            return "Unable to read jokes.txt"
+        }
+        let leaders = {}
+        data = data.replace(/^\s*\n/gm, "").split("\n");
+        data.forEach(line => {
+            if (!(line in leaders)) {
+                leaders[line] = 1;
+            } else {
+                leaders[line] += 1;
+            }
+        })
+        let max = 0;
+        Object.entries(leaders).forEach(entry => {
+            let joke = entry[0];
+            let count = entry[1];
+            if (count > max) {
+                max = count;
+                leader = joke;
+            }
+        })
+        console.log(`returning leader I hope... ${leader}`);
+    })
+    return leader;
+}
+
 if (!process.argv[2]) {
     let prompt = require('prompt');
 
     prompt.start();
 
-    prompt.get(['category'], function (err, result) {
+    prompt.get(['category', 'leaderboard'], function (err, result) {
         Promise.resolve(returnResponse(result.category))
         .then(resp => validateResponse(resp))
         .then(joke => saveJoke(joke))
         .catch(error => console.log(error))
+
+        if (result.leaderboard != null) {
+            Promise.resolve(findLeader())
+            .then(leader => console.log(`Most popular joke is ${leader}`))
+            .catch(err => console.log(err));
+        }
     });
 } else {
     const searchTerm = process.argv[2]
