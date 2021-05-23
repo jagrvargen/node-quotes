c74e41d5f23453ac0a34ccad4bce453b8c17e345
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2021-05-23 04:38:33 -0700

    use event emitter instead of promises

--- src/app.js
+++ src/app.js
@@ -1,58 +1,5 @@
-const returnResponse = require('./routes/get-jokes');
+const ee = require('./routes/get-jokes');
 const process = require('process');
-const fs = require('fs');
-const fsPromises = require('fs/promises');
-
-const filename = './jokes.txt'
-
-function randInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-function validateResponse(response) {
-    const jokes = response.data.results
-    if (!jokes || !jokes.length) {
-        throw new Error("No joke found");
-    }
-    return jokes[randInt(jokes.length)].joke
-}
-
-function saveJoke(joke) {
-    fs.appendFile(filename, joke.trim().replace(/\r?\n|\r/g, " ") + "\n", (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log(joke)
-    });
-}
-
-function findLeader() {
-    return fsPromises.readFile(filename, encoding='utf8')
-    .then(data => {
-        let leaders = {}
-        data = data.replace(/^\s*\n/gm, "").split("\n");
-        data.forEach(line => {
-            if (!(line in leaders)) {
-                leaders[line] = 1;
-            } else {
-                leaders[line] += 1;
-            }
-        })
-        let max = 0;
-        Object.entries(leaders).forEach(entry => {
-            let joke = entry[0];
-            let count = entry[1];
-            if (count > max) {
-                max = count;
-                leader = joke;
-            }
-        })
-        return leader;
-    })
-    .catch(err => {
-        return "Unable to read jokes.txt"
-    })
-}
 
 if (!process.argv[2]) {
     let prompt = require('prompt');
@@ -60,21 +7,16 @@ if (!process.argv[2]) {
     prompt.start();
 
     prompt.get(['category', 'leaderboard'], function (err, result) {
-        Promise.resolve(returnResponse(result.category))
-        .then(resp => validateResponse(resp))
-        .then(joke => saveJoke(joke))
-        .catch(error => console.log(error))
+        ee.emit('returnResponse', result.category)
 
         if (result.leaderboard) {
-            findLeader()
-            .then(leader => console.log(`Most popular joke is ${leader}`))
-            .catch(err => console.log(err));
+            ee.emit('findLeader')
         }
     });
 } else {
     const searchTerm = process.argv[2]
-    returnResponse(searchTerm)
-        .then(resp => validateResponse(resp))
-        .then(joke => saveJoke(joke))
-        .catch(error => console.log(error))
+    ee.emit('returnResponse', searchTerm);
+    if (process.argv[3] && process.argv[3] === "leader") {
+        ee.emit('findLeader')
+    }
 }
--- src/routes/get-jokes.js
+++ src/routes/get-jokes.js
@@ -1,7 +1,69 @@
 const axios = require('axios').default;
 const querystring = require('querystring');
+const emitter = require('events').EventEmitter;
+const fs = require('fs');
+const fsPromises = require('fs/promises');
 
 const url = 'https://icanhazdadjoke.com/';
+const filename = './jokes.txt'
+
+const ee = new emitter();
+
+function randInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+var joke;
+
+ee.on('validateResponse', (response) => {
+    const jokes = response.data.results
+    if (!jokes || !jokes.length) {
+        throw new Error("No joke found");
+    }
+    joke = jokes[randInt(jokes.length)].joke
+    ee.emit('saveJoke', joke)
+})
+
+ee.on('saveJoke', (joke) => {
+    fs.appendFile(filename, joke.trim().replace(/\r?\n|\r/g, " ") + "\n", (err) => {
+        if (err) {
+            console.log(err)
+            throw err;
+        } else {
+            console.log('Reading file')
+            fs.readFileSync(filename, 'utf8')
+        }
+        console.log(joke)
+    });
+})
+
+ee.on('findLeader', () => {
+    return fsPromises.readFile(filename, encoding='utf8')
+    .then(data => {
+        let leaders = {}
+        data = data.replace(/^\s*\n/gm, "").split("\n");
+        data.forEach(line => {
+            if (!(line in leaders)) {
+                leaders[line] = 1;
+            } else {
+                leaders[line] += 1;
+            }
+        })
+        let max = 0;
+        Object.entries(leaders).forEach(entry => {
+            let joke = entry[0];
+            let count = entry[1];
+            if (count > max) {
+                max = count;
+                leader = joke;
+            }
+        })
+        console.log(`leader is ${leader}`)
+    })
+    .catch(err => {
+        return "Unable to read jokes.txt"
+    })
+})
 
 const jokes = axios.create({
     baseURL: url,
@@ -11,17 +73,21 @@ const jokes = axios.create({
     }
 });
 
-function getJoke(searchTerm) {
-    if (!searchTerm || searchTerm === "") {
-        return jokes();
-    }
-    return jokes(url + 'search?' + querystring.stringify({term: searchTerm}));
-}
+var response;
 
-module.exports = function returnResponse(searchTerm) {
-    return new Promise(function (resolve, reject) {
-        let jokeJSON = getJoke(searchTerm)
-        resolve(jokeJSON)
-        reject("error")
-    })
-}
+ee.on('getJoke', (searchTerm) => {
+    if (!searchTerm || searchTerm === "") {
+        response = jokes();
+    }
+    response = jokes(url + 'search?' + querystring.stringify({term: searchTerm}));
+})
+
+ee.on('returnResponse', (searchTerm) => {
+    ee.emit('getJoke', searchTerm)
+    response.then(resp => {
+        ee.emit('validateResponse', resp);
+    }).catch(err => console.log(err))
+    return joke;
+})
+
+module.exports = ee
